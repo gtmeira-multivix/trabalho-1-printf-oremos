9dda5f523ddf14baaa5d2a26f13b7f4ef0e106fb
Marcellina <43495251+TiagoEU@users.noreply.github.com>
2018-10-06 23:01:23 -0300

    teste fila e pilha

--- main.c
+++ main.c
@@ -5,15 +5,30 @@
 #include "tokenizacao.h"
 
 int main() {
-	//Substitua esta função pela implementação do seu trabalho.
-	printf("Digite uma expressão:\n");
+	printf("--- Teste pilha ---\n");
+	printf("Digite uma expressao:\n");
+	Pilha *p = pilha_criar();
+	Fila *f = fila_criar();
 	Token t = token_proximo();
-	
-	while(t.tipo != FIM && t.tipo != ERRO) {
-		printf("\nToken = ");
-		token_imprimir(t);
-		t = token_proximo();
+	while(t.tipo != FIM && t.tipo != ERRO) 
+	{
+		//teste pilha
+		pilha_push(p, t);
+		printf("\nToken pilha = ");
+		pilha_imprimir(p);	
+		t = token_proximo();			
 	}
-	
+	printf("\n\n================== ");
+	printf("\n\n--- Teste fila ---\n");
+	printf("Digite uma expressao:\n");
+	Token a = token_proximo();
+	while(a.tipo != FIM && a.tipo != ERRO) 
+	{
+		//teste fila
+		printf("\nToken fila = ");
+		fila_adicionar(f, a);
+		fila_imprimir(f);
+		a = token_proximo();		
+	}	
 	return 0;
 }
